d227e60f86d3030985839dad2736490c57f57d6b
mehmet.pekmezci.32@gmail.com <mehmet.pekmezci.32@gmail.com@bdb34082-8c07-7120-601e-5387562cd7a9>
2011-12-08 21:11:59 +0000

    MP

--- trunk/PredictionServer/src/tr/org/predictionserver/neural/backprop/BackPropPredictor.java
+++ trunk/PredictionServer/src/tr/org/predictionserver/neural/backprop/BackPropPredictor.java
@@ -3,6 +3,7 @@ package tr.org.predictionserver.neural.backprop;
 
 import tr.org.predictionserver.neural.Prediction;
 import tr.org.predictionserver.neural.Predictor;
+import tr.org.predictionserver.neural.crbm.Matrix;
 
 public class BackPropPredictor implements Predictor{
 	private final static int EPOCHS=400;
@@ -24,7 +25,7 @@ public class BackPropPredictor implements Predictor{
 		long startTime=System.currentTimeMillis();
 		double minimumNetError=Double.MAX_VALUE;
 		for(int i=0;i<EPOCHS;i++){
-			for(int j=0;j<inputData.length-timeWindowWidth-outputUnitsSize;j++){
+			for(int j=0;j<inputData.length-timeWindowWidth-outputUnitsSize+1;j++){
 				double[] timeWindow=new double[timeWindowWidth];
 				System.arraycopy(inputData, j, timeWindow, 0, timeWindowWidth);
 				double[] targetWindow=new double[outputUnitsSize];
@@ -43,14 +44,29 @@ public class BackPropPredictor implements Predictor{
 			}
 		}
 		
+		
+		
+		
+		
 		double[] lastTimeWindow=new double[timeWindowWidth];
 		System.arraycopy(inputData, inputData.length-timeWindowWidth, lastTimeWindow, 0, timeWindowWidth);
 		network.restoreWeights();
 		returnValue.networkError=network.savedError;
-		network.setInput(lastTimeWindow);
-		network.propagate();
-		returnValue.networkError=network.computeOutputError(lastTimeWindow);
-		returnValue.predictedValues=network.getOutput();
+		
+		
+		
+		for(int time=0;time<timeWindowWidth;time++){
+			network.setInput(lastTimeWindow);
+			network.propagate();
+			returnValue.predictedValues=network.getOutput();
+			for(int i=0;i<timeWindowWidth-1;i++){
+				lastTimeWindow[i]=lastTimeWindow[i+1];
+			}
+			lastTimeWindow[lastTimeWindow.length-1]=returnValue.predictedValues[0];
+		}
+		
+		
+		
 		returnValue.totoalTimeMillis=System.currentTimeMillis()-startTime;
 		return returnValue;
 	}
--- trunk/PredictionServer/src/tr/org/predictionserver/test/Test.java
+++ trunk/PredictionServer/src/tr/org/predictionserver/test/Test.java
@@ -21,38 +21,41 @@ public class Test {
 					ArrayList<PredictionResult> predictionResults=new ArrayList<PredictionResult>();
 					
 					for(int dataSetNo=0;dataSetNo<DataProvider.getDataSets().size(); dataSetNo++){
-						// 70 tane veri olsun, bunda enson 54'e  kadar gideriz cunku
-						//                 54'te  5 tanesi input ve 1 tanesi test icin etti 60.
-						//                 60'ta : 5 input veriyoruz 60-65 arası.
-						//                              son 5 gerçek karşılaştırma deferi
-					    //                              5 tane de neural net predict eder, bu son ikisinin farkını error olarak predictionresult'a yazılır. 
-						// 70 tane veri= 1 kaynaktan 70 ölçüm yapıyoruz,
-						// t=0 0-5 arasını neural network'e veriyoruz, , t=2'de 1-6 arasını eğitiyoruz.
-						
-						// son 5 tanenin tahmini su sekilde olacak : 
-						// 66. tahmin edilir, 
-						//			          time window 1 kaydırılır, bilinen input 5'li sinin son degeri 66. tahmin edilen noktadır, 67. tahmin edilir, 
-						//			          time window 1 kaydırılır, bilinen input 5'li sinin sondan bir onceki degeri 666. tahmin , son degeri 67. tahmin edilen noktadır, 68. tahmin edilir
-						// ....., yani ilk tahnminden sonraki tahminlere , daha önceki tahminler input data olur..
+						for(int tryTheSameDataNumber=0;tryTheSameDataNumber<5;tryTheSameDataNumber++){
+							// 70 tane veri olsun, bunda enson 54'e  kadar gideriz cunku
+							//                 54'te  5 tanesi input ve 1 tanesi test icin etti 60.
+							//                 60'ta : 5 input veriyoruz 60-65 arası.
+							//                              son 5 gerçek karşılaştırma deferi
+						    //                              5 tane de neural net predict eder, bu son ikisinin farkını error olarak predictionresult'a yazılır. 
+							// 70 tane veri= 1 kaynaktan 70 ölçüm yapıyoruz,
+							// t=0 0-5 arasını neural network'e veriyoruz, , t=2'de 1-6 arasını eğitiyoruz.
+							
+							// son 5 tanenin tahmini su sekilde olacak : 
+							// 66. tahmin edilir, 
+							//			          time window 1 kaydırılır, bilinen input 5'li sinin son degeri 66. tahmin edilen noktadır, 67. tahmin edilir, 
+							//			          time window 1 kaydırılır, bilinen input 5'li sinin sondan bir onceki degeri 666. tahmin , son degeri 67. tahmin edilen noktadır, 68. tahmin edilir
+							// ....., yani ilk tahnminden sonraki tahminlere , daha önceki tahminler input data olur..
 
-						BackPropPredictor backPropPredictor=new BackPropPredictor(neuralNetworkInputUnitsSize,neuralNetworkOutputUnitsSize,timeWindowWidth);
-						ConditionalRBMPredictor  conditionalRBMPredictor=new ConditionalRBMPredictor(neuralNetworkInputUnitsSize,neuralNetworkOutputUnitsSize,timeWindowWidth);
-						PredictionResult predictionResult=new PredictionResult();
-						double[] sampleData=DataProvider.getDataSets().get(dataSetNo);
-						predictionResult.dataSetLength=sampleData.length;
-						predictionResult.inputData=new double[sampleData.length-timeWindowWidth];
-						 System.arraycopy(sampleData, 0, predictionResult.inputData, 0, sampleData.length-timeWindowWidth);
-						 predictionResult.dataToBePredicted=new double[timeWindowWidth];
-					     System.arraycopy(predictionResult.inputData, predictionResult.inputData.length-timeWindowWidth, predictionResult.dataToBePredicted, 0, timeWindowWidth);
-					     predictionResult.dataSetNumber=dataSetNo;
-					  // inputData'nın son 5 tanesi sanki hic yokmus gibi  CRBM ve BackProp tarafından tahmin edilecektir.
-					     // daha sonra da tahmin degerleri asagidaki  gercek son 5 degerle karsilastirilmak uzere PredictionResult'in icine konacaktir.
-					     predictionResult.crbmPrediction=conditionalRBMPredictor.predict( predictionResult.inputData);
-					     predictionResult.crbmPrediction.testError=calculateError(predictionResult.crbmPrediction.predictedValues, predictionResult.dataToBePredicted);
-					     predictionResult.backPropPrediction=backPropPredictor.predict( predictionResult.inputData);
-					     predictionResult.backPropPrediction.testError=calculateError(predictionResult.backPropPrediction.predictedValues, predictionResult.dataToBePredicted);
-					     predictionResults.add(predictionResult);
-					     System.out.println(predictionResult.toString());
+							BackPropPredictor backPropPredictor=new BackPropPredictor(neuralNetworkInputUnitsSize,neuralNetworkOutputUnitsSize,timeWindowWidth);
+							ConditionalRBMPredictor  conditionalRBMPredictor=new ConditionalRBMPredictor(neuralNetworkInputUnitsSize,neuralNetworkOutputUnitsSize,timeWindowWidth);
+							PredictionResult predictionResult=new PredictionResult();
+							predictionResult.tryNumber=tryTheSameDataNumber;
+							double[] sampleData=DataProvider.getDataSets().get(dataSetNo);
+							predictionResult.dataSetLength=sampleData.length;
+							predictionResult.inputData=new double[sampleData.length-timeWindowWidth];
+							 System.arraycopy(sampleData, 0, predictionResult.inputData, 0, sampleData.length-timeWindowWidth);
+							 predictionResult.dataToBePredicted=new double[timeWindowWidth];
+						     System.arraycopy(predictionResult.inputData, predictionResult.inputData.length-timeWindowWidth, predictionResult.dataToBePredicted, 0, timeWindowWidth);
+						     predictionResult.dataSetNumber=dataSetNo;
+						  // inputData'nın son 5 tanesi sanki hic yokmus gibi  CRBM ve BackProp tarafından tahmin edilecektir.
+						     // daha sonra da tahmin degerleri asagidaki  gercek son 5 degerle karsilastirilmak uzere PredictionResult'in icine konacaktir.
+						     predictionResult.crbmPrediction=conditionalRBMPredictor.predict( predictionResult.inputData);
+						     predictionResult.crbmPrediction.testError=calculateError(predictionResult.crbmPrediction.predictedValues, predictionResult.dataToBePredicted);
+						     predictionResult.backPropPrediction=backPropPredictor.predict( predictionResult.inputData);
+						     predictionResult.backPropPrediction.testError=calculateError(predictionResult.backPropPrediction.predictedValues, predictionResult.dataToBePredicted);
+						     predictionResults.add(predictionResult);
+						     System.out.println(predictionResult.toString());
+						}
 					}
 					StringBuffer outputFileBuffer=new StringBuffer();
 					for(int i=0;i<predictionResults.size();i++){
